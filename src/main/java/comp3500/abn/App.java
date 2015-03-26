d135c3b852b7152485b97b638d2264fed508bf3b
Benjamin Roberts <benjamin.roberts@anu.edu.au>
2015-03-26 13:22:30 +1100

    instantiated a bare mvn project in the repo. moved net.ripe package into the main artifact location removed eclipse project for time being

---
+++ src/main/java/comp3500/abn/App.java
@@ -0,0 +1,13 @@
+package comp3500.abn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
