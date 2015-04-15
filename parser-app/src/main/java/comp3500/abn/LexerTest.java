1c7d105cbe924ce0ec2d0649ebe009528b3daaee
Benjamin Roberts <benjamin.roberts@anu.edu.au>
2015-04-15 17:18:26 +1000

    lexer wrapper returns something similar to an AST for the parsed text

--- parser-app/src/main/java/comp3500/abn/LexerTest.java
+++ parser-app/src/main/java/comp3500/abn/LexerTest.java
@@ -7,19 +7,20 @@ import java.io.StringReader;
 
 import org.apache.commons.io.input.ReaderInputStream;
 
+import comp3500.abn.rpsl.AttributeLexerWrapper;
+
 import net.ripe.db.whois.common.generated.ImportLexer;
 
 public class LexerTest {
-	private static String input = "import:   from AS4746\n" + 
-			"          action pref=100;\n" + 
-			"          accept AS7569 AS4776 AS7570 AS7572 AS4738 AS7571 AS7574 AS7573 AS1221 AS7474 AS4775"; 
+	private static String input = "    import: from AS2 7.7.7.2 at 7.7.7.1 action pref = 1; action pref = 1.5;\n" + 
+			"            from AS2                    action pref = 2;\n" + 
+			"            accept AS4"; 
 	
 	public static void main(String args[]) throws IOException, ClassNotFoundException {
-		Reader in = new StringReader(input);
+		Reader in = new StringReader(input),n = null;
 		
-		Lexer lexer = new Lexer("import", in);
+		AttributeLexerWrapper lexer = new AttributeLexerWrapper("import");
 		System.out.println("Input: " + input);
-		System.out.println("States: " + lexer.stateTable);
-		System.out.println("Match Table: " + lexer.parse());
+		System.out.println("Match Table (context): " + lexer.parse(in));
 	}
 }
