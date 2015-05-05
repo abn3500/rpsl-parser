af164e88793a581da9518c15165069af7e9daa3b
Benjamin Roberts <benjamin.roberts@anu.edu.au>
2015-05-05 16:35:47 +1000

    Added static variant of AttribtueLexerWrapper#parse for use with RpslAttribute objects

--- src/test/java/comp3500/abn/rpsl/AttributeLexerWrapperTest.java
+++ src/test/java/comp3500/abn/rpsl/AttributeLexerWrapperTest.java
@@ -6,15 +6,22 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.util.List;
 
+import net.ripe.db.whois.common.rpsl.AttributeType;
+import net.ripe.db.whois.common.rpsl.RpslAttribute;
+
 import org.apache.commons.lang3.tuple.Pair;
 import org.junit.Test;
 
 public class AttributeLexerWrapperTest {
+	private static final String importString = 
+			"    import: from AS2 7.7.7.2 at 7.7.7.1 action pref = 1;\n" + 
+			"            from AS2                    action pref = 2;\n" + 
+			"            accept AS4";
 
 	@Test
 	public void loadsExistingLexer() {
 		try {
-			AttributeLexerWrapper a = new AttributeLexerWrapper("import");
+			new AttributeLexerWrapper("import");
 		} catch (ClassNotFoundException e) {
 			fail("Failed to instantiate known lexer: import");
 		}
@@ -23,14 +30,20 @@ public class AttributeLexerWrapperTest {
 	@Test
 	public void parsesImportBlock() throws ClassNotFoundException, IOException{
 		AttributeLexerWrapper importLexer = new AttributeLexerWrapper("import");
-		String importString = "    import: from AS2 7.7.7.2 at 7.7.7.1 action pref = 1;\n" + 
-				"            from AS2                    action pref = 2;\n" + 
-				"            accept AS4";
+
 		StringReader strReader = new StringReader(importString);
 		List<Pair<String,List<String>>> ast = importLexer.parse(strReader);
 		assertEquals("Parser genereated abmormal output",
 				"[(from,[AS2, 7.7.7.2]), (at,[7.7.7.1]), (action,[pref, =, 1]), (from,[AS2]), (action,[pref, =, 2]), (accept,[AS4])]",
 				ast.toString());		
+	}
+	
+	@Test
+	public void parsesImportBlockObject() {
+		RpslAttribute attr = new RpslAttribute(AttributeType.IMPORT, importString);
+		assertEquals("Parser genereated abmormal output",
+				"[(from,[AS2, 7.7.7.2]), (at,[7.7.7.1]), (action,[pref, =, 1]), (from,[AS2]), (action,[pref, =, 2]), (accept,[AS4])]",
+				AttributeLexerWrapper.parse(attr).toString());	
 	}
 
 }
